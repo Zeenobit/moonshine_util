3764aafe156337496296bf1527c95d10793cf9eb
Zeenobit <zeenobit.exe@gmail.com>
2024-03-09 12:09:46 -0500

    Update ecs.rs

--- src/ecs.rs
+++ src/ecs.rs
@@ -10,37 +10,38 @@ pub trait RunSystemLoop: Sized {
         n: usize,
         system: T,
     ) -> Vec<Out> {
-        self.run_system_loop_with(n, (), system)
+        self.run_system_loop_with(n, || (), system)
     }
 
     /// Runs a system `n` times with the given input, returning a vector of the outputs.
-    fn run_system_loop_with<T: IntoSystem<In, Out, Marker>, In, Out, Marker>(
+    fn run_system_loop_with<InputSource, T: IntoSystem<In, Out, Marker>, In, Out, Marker>(
         self,
         n: usize,
-        input: In,
+        input: InputSource,
         system: T,
     ) -> Vec<Out>
     where
-        In: Clone;
+        InputSource: Fn() -> In;
 }
 
 impl RunSystemLoop for &mut World {
-    fn run_system_loop_with<T: IntoSystem<In, Out, Marker>, In, Out, Marker>(
+    fn run_system_loop_with<InputSource, T: IntoSystem<In, Out, Marker>, In, Out, Marker>(
         self,
         n: usize,
-        input: In,
+        input: InputSource,
         system: T,
     ) -> Vec<Out>
     where
-        In: Clone,
+        InputSource: Fn() -> In,
     {
         let mut system: T::System = IntoSystem::into_system(system);
         system.initialize(self);
-        let mut out = Vec::new();
+        let mut outs = Vec::new();
         for _ in 0..n {
-            out.push(system.run(input.clone(), self));
+            let out = system.run(input(), self);
+            outs.push(out);
+            system.apply_deferred(self);
         }
-        system.apply_deferred(self);
-        out
+        outs
     }
 }
