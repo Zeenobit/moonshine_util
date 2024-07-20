3e16d409caddabfc83e0a0c979741d6d59bb456b
Zeenobit <zeenobit.exe@gmail.com>
2024-07-20 02:21:54 -0400

    Add `Future` and `Promise`

---
+++ src/future.rs
@@ -0,0 +1,55 @@
+use std::sync::{Arc, Mutex};
+
+#[must_use]
+pub struct Promise<T>(Arc<Mutex<Option<T>>>);
+
+impl<T> Promise<T> {
+    pub fn new() -> Self {
+        Self(Arc::new(Mutex::new(None)))
+    }
+
+    pub fn done(self, value: T) {
+        *self.0.lock().unwrap() = Some(value);
+    }
+}
+
+impl<T> Default for Promise<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[must_use]
+pub struct Future<T>(Arc<Mutex<Option<T>>>);
+
+impl<T> Future<T> {
+    pub fn new(promise: &Promise<T>) -> Self {
+        Self(Arc::clone(&promise.0))
+    }
+
+    pub fn poll(self) -> FutureResponse<T> {
+        let value = self.0.lock().unwrap().take();
+        match value {
+            Some(value) => FutureResponse::Done(value),
+            None => FutureResponse::Wait(self),
+        }
+    }
+}
+
+pub enum FutureResponse<T> {
+    Wait(Future<T>),
+    Done(T),
+}
+
+impl<T> FutureResponse<T> {
+    pub fn is_done(&self) -> bool {
+        matches!(self, Self::Done(_))
+    }
+
+    pub fn unwrap(self) -> T {
+        match self {
+            Self::Done(value) => value,
+            Self::Wait(_) => panic!("future is not ready"),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@
 
 pub mod diagnostics;
 pub mod expect;
+pub mod future;
 pub mod hierarchy;
 pub mod system;
