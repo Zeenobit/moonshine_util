df403713966ade46133bae7835b4e7f896b5fb1c
Zeenobit <zeenobit.exe@gmail.com>
2024-07-23 13:23:50 -0400

    Add `descendants_deep` and `descendants_wide`

--- src/hierarchy.rs
+++ src/hierarchy.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use bevy_ecs::{
     prelude::*,
     query::{QueryData, QueryFilter, QueryItem, ReadOnlyQueryData},
@@ -53,6 +55,7 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
         self.parent.get(entity).ok().map(|parent| **parent)
     }
 
+    #[deprecated]
     /// Returns true if the given entity has a parent.
     pub fn has_parent(&self, entity: Entity) -> bool {
         self.parent(entity).is_some()
@@ -67,6 +70,7 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
             .flat_map(|children| children.into_iter().copied())
     }
 
+    #[deprecated]
     /// Returns true if the given entity has children.
     pub fn has_children(&self, entity: Entity) -> bool {
         self.children
@@ -76,6 +80,7 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
             .unwrap_or(false)
     }
 
+    #[deprecated]
     /// Returns the root of the given entity's hierarchy.
     pub fn root(&self, entity: Entity) -> Entity {
         let mut root = entity;
@@ -85,6 +90,7 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
         root
     }
 
+    #[deprecated]
     /// Returns true if the given `entity` is the root of its hierarchy.
     pub fn is_root(&self, entity: Entity) -> bool {
         self.parent(entity).is_none()
@@ -95,26 +101,40 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
         self.parent.iter_ancestors(entity)
     }
 
+    #[deprecated(note = "use `descendants_wide` instead")]
     /// Iterates over the descendants of the given `entity`.
     pub fn descendants(&self, entity: Entity) -> impl Iterator<Item = Entity> + '_ {
+        self.descendants_wide(entity)
+    }
+
+    /// Iterates over the descendants of the given `entity`.
+    pub fn descendants_wide(&self, entity: Entity) -> impl Iterator<Item = Entity> + '_ {
         self.children.iter_descendants(entity)
     }
 
+    pub fn descendants_deep(&self, entity: Entity) -> impl Iterator<Item = Entity> + '_ {
+        DeepDescendantIter::new(&self.children, entity)
+    }
+
+    #[deprecated]
     /// Returns true if given `entity` is an ancestor of the given `descendant`.
     pub fn is_ancestor_of(&self, entity: Entity, descendant: Entity) -> bool {
         self.ancestors(descendant).any(|parent| parent == entity)
     }
 
+    #[deprecated]
     /// Returns true if given `entity` is a child of the given `parent`.
     pub fn is_child_of(&self, entity: Entity, parent: Entity) -> bool {
         self.parent(entity).map(|p| p == parent).unwrap_or(false)
     }
 
+    #[deprecated]
     /// Returns true if given `entity` is a descendant of the given `ancestor`.
     pub fn is_descendant_of(&self, entity: Entity, ancestor: Entity) -> bool {
         self.ancestors(entity).any(|parent| parent == ancestor)
     }
 
+    #[deprecated]
     /// Returns the first ancestor of the given `entity` that matches the given `query`.
     pub fn find_ancestor<'a, T: ReadOnlyQueryData, F: QueryFilter>(
         &self,
@@ -125,6 +145,7 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
             .find_map(|ancestor| query.get(ancestor).ok())
     }
 
+    #[deprecated]
     /// Returns the first ancestor of the given `entity` that matches the given mutable `query`.
     pub fn find_ancestor_mut<'a, T: QueryData, F: QueryFilter>(
         &self,
@@ -140,17 +161,50 @@ impl<'w, 's> HierarchyQuery<'w, 's> {
         None
     }
 
+    #[deprecated]
     /// Returns the first descendant of the given `entity` that matches the given `query`.
     pub fn find_descendant<'a, T: ReadOnlyQueryData, F: QueryFilter>(
         &self,
         entity: Entity,
         query: &'a Query<T, F>,
     ) -> Option<QueryItem<'a, T::ReadOnly>> {
-        self.descendants(entity)
+        self.descendants_wide(entity)
             .find_map(|descendant| query.get(descendant).ok())
     }
+}
 
-    // TODO: find_descendant_mut
+/// An [`Iterator`] of [`Entity`]s over the descendants of an [`Entity`].
+///
+/// Traverses the hierarchy breadth-first.
+struct DeepDescendantIter<'w, 's, 'a> {
+    query: &'a Query<'w, 's, &'static Children>,
+    stack: Vec<Entity>,
+}
+
+impl<'w, 's, 'a> DeepDescendantIter<'w, 's, 'a> {
+    fn new(query: &'a Query<'w, 's, &'static Children>, entity: Entity) -> Self {
+        DeepDescendantIter {
+            query,
+            stack: query
+                .get(entity)
+                .into_iter()
+                .flatten()
+                .copied()
+                .rev()
+                .collect(),
+        }
+    }
+}
+
+impl Iterator for DeepDescendantIter<'_, '_, '_> {
+    type Item = Entity;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.stack.pop()?;
+        let children = self.query.get(current).into_iter().flatten().copied().rev();
+        self.stack.extend(children);
+        Some(current)
+    }
 }
 
 #[cfg(test)]
@@ -170,8 +224,6 @@ mod tests {
             .id();
 
         let pass = w.run_system_once(move |q: HierarchyQuery| {
-            assert!(q.is_root(e));
-            assert!(q.has_children(e));
             assert_eq!(q.children(e).count(), 1);
             true
         });
@@ -180,45 +232,107 @@ mod tests {
     }
 
     #[test]
-    fn query() {
+    fn ancestors() {
         #[derive(Component)]
-        struct A;
+        struct A(usize);
 
         #[derive(Component)]
-        struct B;
-
-        #[derive(Component)]
-        struct C;
+        struct X;
 
         let mut w = World::new();
-        let a = w
-            .spawn(A)
+        w.spawn(A(0)).with_children(|a| {
+            a.spawn(A(1)).with_children(|b| {
+                b.spawn(A(2)).with_children(|c| {
+                    c.spawn(A(3));
+                    c.spawn(A(4)).with_children(|d| {
+                        d.spawn(A(5));
+                    });
+                });
+                b.spawn(A(6)).with_children(|c| {
+                    c.spawn((A(7), X));
+                });
+            });
+        });
+
+        let r = w.run_system_once(
+            move |q: HierarchyQuery, qa: Query<&A>, qx: Query<Entity, With<X>>| {
+                let entity = qx.single();
+                let mut r = Vec::new();
+                for e in q.ancestors(entity) {
+                    r.push(qa.get(e).unwrap().0);
+                }
+                r
+            },
+        );
+
+        assert_eq!(r, vec![6, 1, 0]);
+    }
+
+    #[test]
+    fn descendants_wide() {
+        #[derive(Component)]
+        struct A(usize);
+
+        let mut w = World::new();
+        let entity = w
+            .spawn(A(0))
             .with_children(|a| {
-                a.spawn(B).with_children(|b| {
-                    b.spawn(C);
+                a.spawn(A(1)).with_children(|b| {
+                    b.spawn(A(2)).with_children(|c| {
+                        c.spawn(A(3));
+                        c.spawn(A(4)).with_children(|d| {
+                            d.spawn(A(5));
+                        });
+                    });
+                    b.spawn(A(6)).with_children(|c| {
+                        c.spawn(A(7));
+                    });
                 });
             })
             .id();
 
-        let pass = w.run_system_once(
-            move |q: HierarchyQuery,
-                  qa: Query<Entity, With<A>>,
-                  qb: Query<Entity, With<B>>,
-                  qc: Query<Entity, With<C>>| {
-                let c_from_a = q.find_descendant(a, &qc).unwrap();
-                let b_from_c = q.find_ancestor(c_from_a, &qb).unwrap();
-                let b_from_a = q.find_descendant(a, &qb).unwrap();
-                let a_from_c = q.find_ancestor(c_from_a, &qa).unwrap();
-                let a_from_b = q.find_ancestor(b_from_c, &qa).unwrap();
-                let c_from_b = q.find_descendant(b_from_a, &qc).unwrap();
-                assert_eq!(c_from_a, c_from_b);
-                assert_eq!(b_from_a, b_from_c);
-                assert_eq!(a_from_c, a_from_b);
-                assert_eq!(a_from_c, a);
-                true
-            },
-        );
+        let r = w.run_system_once(move |q: HierarchyQuery, qa: Query<&A>| {
+            let mut r = Vec::new();
+            for e in q.descendants_wide(entity) {
+                r.push(qa.get(e).unwrap().0);
+            }
+            r
+        });
 
-        assert!(pass);
+        assert_eq!(r, vec![1, 2, 6, 3, 4, 7, 5]);
+    }
+
+    #[test]
+    fn descendants_deep() {
+        #[derive(Component)]
+        struct A(usize);
+
+        let mut w = World::new();
+        let entity = w
+            .spawn(A(0))
+            .with_children(|a| {
+                a.spawn(A(1)).with_children(|b| {
+                    b.spawn(A(2)).with_children(|c| {
+                        c.spawn(A(3));
+                        c.spawn(A(4)).with_children(|d| {
+                            d.spawn(A(5));
+                        });
+                    });
+                    b.spawn(A(6)).with_children(|c| {
+                        c.spawn(A(7));
+                    });
+                });
+            })
+            .id();
+
+        let r = w.run_system_once(move |q: HierarchyQuery, qa: Query<&A>| {
+            let mut r = Vec::new();
+            for e in q.descendants_deep(entity) {
+                r.push(qa.get(e).unwrap().0);
+            }
+            r
+        });
+
+        assert_eq!(r, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 }
