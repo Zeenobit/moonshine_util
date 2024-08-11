5dc4a18515e07b4f0a2bd33dca6f8bfc350955f3
Zeenobit <zeenobit.exe@gmail.com>
2024-08-11 02:03:04 -0400

    Add `PluginFn`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 #![doc = include_str!("../README.md")]
 
+pub mod app;
 pub mod diagnostics;
 pub mod expect;
 pub mod future;
 pub mod hierarchy;
 pub mod system;
+
+pub mod prelude {
+    pub use crate::{
+        app::{AddPluginFn, AddPluginFnToGroup, FnPlugin},
+        expect::Expect,
+        system::*,
+    };
+}
