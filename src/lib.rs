711c544efc08c5706acca7889022a0b7ae98f2ef
Zeenobit <zeenobit.exe@gmail.com>
2024-08-18 12:49:44 -0400

    Revert "Add `PluginFn`"
    This reverts commit 5dc4a18515e07b4f0a2bd33dca6f8bfc350955f3.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
 #![doc = include_str!("../README.md")]
 
-pub mod app;
 pub mod diagnostics;
 pub mod expect;
 pub mod future;
 pub mod hierarchy;
 pub mod system;
-
-pub mod prelude {
-    pub use crate::{
-        app::{AddPluginFn, AddPluginFnToGroup, FnPlugin},
-        expect::Expect,
-        system::*,
-    };
-}
