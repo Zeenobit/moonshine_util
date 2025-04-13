caa077971572c8fe47dacd7d55cce97d70615423
Zeenobit <zeenobit.exe@gmail.com>
2025-04-13 14:09:37 -0400

    Add `Get<T>` and `FromQuery`

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,13 @@ pub mod diagnostics;
 pub mod expect;
 pub mod future;
 pub mod hierarchy;
+pub mod query;
 pub mod system;
 
 pub mod prelude {
-    pub use crate::{expect::Expect, system::*};
+    pub use crate::expect::Expect;
+    pub use crate::query::{FromQuery, Get};
+    pub use crate::system::*;
 }
 
 pub fn get_short_name(name: &str) -> String {
---
+++ src/query.rs
@@ -0,0 +1,89 @@
+use std::marker::PhantomData;
+
+use bevy_ecs::archetype::Archetype;
+use bevy_ecs::component::{ComponentId, Components, Tick};
+use bevy_ecs::prelude::*;
+use bevy_ecs::query::{FilteredAccess, QueryData, ReadOnlyQueryData, WorldQuery};
+use bevy_ecs::storage::{Table, TableRow};
+
+pub trait FromQuery {
+    type Query: ReadOnlyQueryData;
+
+    fn map(data: <Self::Query as WorldQuery>::Item<'_>) -> Self;
+}
+
+// TODO: Documentation (Experimental!)
+pub struct Get<T>(PhantomData<T>);
+
+unsafe impl<T: FromQuery> WorldQuery for Get<T> {
+    type Item<'a> = T;
+
+    type Fetch<'a> = <T::Query as WorldQuery>::Fetch<'a>;
+
+    type State = <T::Query as WorldQuery>::State;
+
+    fn shrink<'wlong: 'wshort, 'wshort>(item: Self::Item<'wlong>) -> Self::Item<'wshort> {
+        item
+    }
+
+    fn shrink_fetch<'wlong: 'wshort, 'wshort>(fetch: Self::Fetch<'wlong>) -> Self::Fetch<'wshort> {
+        T::Query::shrink_fetch(fetch)
+    }
+
+    unsafe fn init_fetch<'w>(
+        world: bevy_ecs::world::unsafe_world_cell::UnsafeWorldCell<'w>,
+        state: &Self::State,
+        last_run: Tick,
+        this_run: Tick,
+    ) -> Self::Fetch<'w> {
+        unsafe { T::Query::init_fetch(world, state, last_run, this_run) }
+    }
+
+    const IS_DENSE: bool = T::Query::IS_DENSE;
+
+    unsafe fn set_archetype<'w>(
+        fetch: &mut Self::Fetch<'w>,
+        state: &Self::State,
+        archetype: &'w Archetype,
+        table: &'w Table,
+    ) {
+        unsafe { T::Query::set_archetype(fetch, state, archetype, table) }
+    }
+
+    unsafe fn set_table<'w>(fetch: &mut Self::Fetch<'w>, state: &Self::State, table: &'w Table) {
+        unsafe { T::Query::set_table(fetch, state, table) }
+    }
+
+    unsafe fn fetch<'w>(
+        fetch: &mut Self::Fetch<'w>,
+        entity: Entity,
+        table_row: TableRow,
+    ) -> Self::Item<'w> {
+        T::map(T::Query::fetch(fetch, entity, table_row))
+    }
+
+    fn update_component_access(state: &Self::State, access: &mut FilteredAccess<ComponentId>) {
+        T::Query::update_component_access(state, access)
+    }
+
+    fn init_state(world: &mut World) -> Self::State {
+        T::Query::init_state(world)
+    }
+
+    fn get_state(components: &Components) -> Option<Self::State> {
+        T::Query::get_state(components)
+    }
+
+    fn matches_component_set(
+        state: &Self::State,
+        set_contains_id: &impl Fn(ComponentId) -> bool,
+    ) -> bool {
+        T::Query::matches_component_set(state, set_contains_id)
+    }
+}
+
+unsafe impl<T: FromQuery> QueryData for Get<T> {
+    type ReadOnly = Self;
+}
+
+unsafe impl<T: FromQuery> ReadOnlyQueryData for Get<T> {}
